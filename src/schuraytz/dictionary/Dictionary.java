67c5791c199f6a76d36a2e8fe8788ca54f21699f
sschuraytz <sbschuraytz@gmail.com>
2019-02-26 16:52:45 -0500

    added method to check if word is palindrom, method to return a list of all palindromes in dictionary, and junit tests for those 2 methods and the dictionary size method

--- src/schuraytz/dictionary/Dictionary.java
+++ src/schuraytz/dictionary/Dictionary.java
@@ -7,11 +7,13 @@ import java.util.Scanner;
 
 public class Dictionary {
 
-    ArrayList list = new ArrayList();
+   private ArrayList<String> list = new ArrayList();
 
 
     public Dictionary() throws FileNotFoundException {
-            Scanner scanner = new Scanner(new File("dictionary.txt"));
+            String path = "C://Users//SB//Documents//LCW//Semester 4 (Spring 2019)//Methodology//simpleDictionary.txt";
+            // String path = "dictionary.txt";
+            Scanner scanner = new Scanner(new File(path));
             while (scanner.hasNext()) {
                 String word = scanner.next();
                 list.add(word);
@@ -19,19 +21,38 @@ public class Dictionary {
             }
             scanner.close();
     }
-        public boolean contains (String word){
-            return list.contains(word.toUpperCase());
-        }
 
-        public boolean isPalindrome (String word) {
-            return false;
-        }
-
-        public List<String> getPalindrome () {
-            return null;
-        }
-
-        public int size() {
-            return  list.size();
-        }
+    public boolean contains (String word){
+        return list.contains(word.toUpperCase());
     }
+
+    public boolean isPalindrome (String word) {
+
+        String reverse = new StringBuilder(word).reverse().toString();
+
+        char[] letters = word.toCharArray();
+        char[] reverseLetters = reverse.toCharArray();
+
+        for (char i = 0; i < letters.length; i++) {
+            if (letters[i] != reverseLetters[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // return all the palindromes in the dictionary
+    public List<String> getPalindromes () {
+        ArrayList palindromeList = new ArrayList();
+        for (String word : list) {
+            if (isPalindrome(word)) {
+                palindromeList.add(word);
+            }
+        }
+        return palindromeList;
+    }
+
+    public int size() {
+        return  list.size();
+    }
+}
--- test/schuraytz/dictionary/dictionaryTest.java
+++ test/schuraytz/dictionary/dictionaryTest.java
@@ -3,10 +3,63 @@ package schuraytz.dictionary;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class dictionaryTest {
+
+    @Test
+    /* this test works b/c I created a small file with only 3 words (2 palindromes) & tested that to be sure
+    the functionality works before applying it to the large dictionary file
+    */
+    public void get_dict_palindromes() throws FileNotFoundException {
+        //given
+        Dictionary dictionary = new Dictionary();
+
+        //when
+        ArrayList palindromes = new ArrayList();
+        palindromes.add("anna");
+        palindromes.add("mom");
+
+        //then
+        assertEquals(palindromes, dictionary.getPalindromes());
+    }
+
+    @Test
+    public void get_size() throws FileNotFoundException {
+
+        //given
+        Dictionary dictionary = new Dictionary();
+        //when
+
+        //then
+        assertEquals(167964, dictionary.size());
+    }
+
+    @Test
+    public void is_palindrome() throws FileNotFoundException {
+
+        //given
+        Dictionary dictionary = new Dictionary();
+
+        //when
+
+        //then
+        assertTrue(dictionary.isPalindrome("kayak"));
+    }
+
+    @Test
+    public void is_not_palindrome() throws FileNotFoundException {
+
+        //given
+        Dictionary dictionary = new Dictionary();
+
+        //when
+
+        //then
+        assertFalse(dictionary.isPalindrome("hello"));
+    }
 
     @Test
     public void contains_true() throws FileNotFoundException {
